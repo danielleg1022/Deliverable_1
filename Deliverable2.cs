0f548049c76a524c9e8bf47a8911cfef0ea1b917
danie <daniellegriffin728@gmail.com>
2022-07-28 13:14:07 -0400

    made changes to if else statements

--- Deliverable2.cs
+++ Deliverable2.cs
@@ -2,7 +2,7 @@
 
 class Program
 {
-    public static void Main(string[] args)
+    static void Main(string[] args)
     {
         Console.WriteLine("Hi! Welcome to our flavorable buffet. All you can eat for 9.99!. We only charge extra for Pepsi. How many people are in your group? Please, parties of 6 or lower.");
 
@@ -10,23 +10,26 @@ class Program
 
         if (num1 > 6)
         {
-            Console.WriteLine("Oh sorry, we can only seat parties up to 6. HAve a nice day.");
+            Console.WriteLine("Oh sorry, we can only seat parties up to 6. HAve a nice day."); return;
+
+
         }
 
-        else if (num1 <= 6)
+        else if (num1 < 6)
 
         {
             Console.WriteLine("A table for " + (num1) + "!Please follow me and take a seat. Let's get everyone started with some drinks. We've got water or Pepsi.");
+
         }
 
 
 
 
 
-        double drink = 3.00 * num1;
+        double drink = 3.00;
         double buffet = 9.99 * num1;
-        double pepsi = 0; 
-        int water =  0;
+        double pepsi = 0;
+        int water = 0;
         char symbol = '$';
 
 
@@ -34,50 +37,39 @@ class Program
 
         {
 
-            Console.WriteLine(" \n Alright, person number " +(i) + " water or Pepsi? \n");
+            Console.WriteLine(" \n Alright, person number " + (i) + " water or Pepsi? \n");
             string response = (Console.ReadLine());
-            
 
-         
+
+
 
 
             {
 
             }
 
-            if ( response  == "water")
+            if (response == "water")
             {
                 Console.WriteLine("water, good choice! \n");
                 water++;
-               
+
 
 
             }
 
-            else             {
-                Console.WriteLine("Pepsi, ok! \n");
+
+
+            else if (response == "pepsi")
+            {
+                Console.WriteLine("Pepsi, ok! \n", drink * num1);
                 pepsi++;
 
             }
 
-            if (response == "tea")
+            else
             {
-                Console.WriteLine("We dont have that. No tea for you!");
+                Console.WriteLine("We dont have that. Nothing for you!");
 
-
-            }
-
-            
-
-
-
-
-
-        }
-
-            Console.WriteLine("Okay, so that's " + pepsi  +   " pepsi's and "  + water + " waters, I'll be right back. Feel free to grab your food!");
-            Console.WriteLine("Here's your bill! Total price:" + symbol + (double)(buffet + drink));
-            {
 
             }
 
@@ -87,17 +79,23 @@ class Program
 
 
 
-
-
-
-
         }
 
+        Console.WriteLine("Okay, so that's " + pepsi + " pepsi's and " + water + " waters, I'll be right back. Feel free to grab your food!");
+        Console.WriteLine("Here's your bill! Total price:" + symbol + (double)(buffet + drink * num1));
+        {
 
-
-
-
+        }
     }
+}
+
+
+
+
+
+
+
+
 
 
 
